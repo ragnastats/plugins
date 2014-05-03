730f3cf1e6ff847debfdb213823ea7ca1b7c9886
Rachel Fish <itsrachelfish@gmail.com>
2014-05-03 12:53:32 -0600

    Old auto-drop plugin

---
+++ drop/drop.pl
@@ -0,0 +1,51 @@
+package Drop;
+ 
+# Perl includes
+use strict;
+use Data::Dumper;
+use Time::HiRes;
+use List::Util;
+ 
+# Kore includes
+use Settings;
+use Plugins;
+use Network;
+use Globals;
+ 
+our $drop = {};
+ 
+Plugins::register("Auto-drop", "Version 0.1 r1", \&unload);
+my $hooks = Plugins::addHooks(['mainLoop_post', \&loop]);
+ 
+sub unload
+{
+        Plugins::delHooks($hooks);
+}
+ 
+sub loop
+{
+        if(Network::DirectConnection::getState() == Network::IN_GAME)
+        {      
+                if($config{autoDrop})
+                {
+                        my $time = Time::HiRes::time();
+                                               
+                        if($drop->{time} < $time)
+                        {
+                                $drop->{item} = [];
+                       
+                                foreach my $item (@{$char->inventory->getItems()})
+                                {
+                                        push(@{$drop->{item}}, $item->{invIndex});
+                                }
+ 
+                                @{$drop->{item}} = List::Util::shuffle @{$drop->{item}};
+                               
+                                #print(@{$drop->{item}}[0]."n");
+                                Commands::run("drop @{$drop->{item}}[0] 1");
+                                                               
+                                $drop->{time} = $time + 0.05;
+                        }
+                }
+        }
+}
