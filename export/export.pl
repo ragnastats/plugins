63540a2e0dba470d713888949fcad921ca74508c
Rachel Fish <itsrachelfish@gmail.com>
2014-05-06 01:03:19 -0600

    Wow, it actually exports now!

--- export/export.pl
+++ export/export.pl
@@ -6,6 +6,7 @@ use Data::Dumper;
 use Time::HiRes;
 use List::Util;
 use Storable;
+use JSON;
  
 # Kore includes
 use Settings;
@@ -16,12 +17,51 @@ use Utils;
 
 Commands::register(["export", "Export some data!", \&export]);
 
-Plugins::register("Export", "Export some data!", \&export);
+Plugins::register("Export", "Export some data!", \&unload);
 my $hooks = Plugins::addHooks(['mainLoop_post', \&loop]);
 
-sub export
+sub export	
 {
+    $Data::Dumper::Terse = 0;        # Output MORE!
+    $Data::Dumper::Indent = 1;       # Output whitespace
+    $Data::Dumper::Maxdepth = 10;       # Output whitespace
+
+	#print(Dumper(@{$char->inventory->getItems()}));
 	
+	my $export = {"inventory" => []};
+	
+	foreach my $item (@{$char->inventory->getItems()})
+	{
+		print("Name: $item->{name} \n");
+		print("Amount: $item->{amount} \n");
+		print("ID: $item->{nameID} \n");
+		print("Sprite: $item->{sprite_id} \n");
+		print("Type: $item->{type} \n");
+		print("Equip Type: $item->{type_equip} \n");
+		print("Identified: $item->{identified} \n");
+		print("Broken: $item->{broken} \n");
+		print("Bind On Equip: $item->{bindOnEquipType} \n");
+		print("Upgrade: $item->{upgrade} \n");
+		print("Cards: $item->{cards} \n");
+		print("Equipped: $item->{equipped} \n");
+		print("Expire: $item->{cards} \n");
+		print("Actor Type: $item->{actorType} \n");
+		print("=============\n");
+
+		push(@{$export->{inventory}}, {item => $item->{nameID}, quantity => $item->{amount}});
+	
+#		print(" - ".$item->{name}." \n - Inv Index: ".$item->{invIndex}." \n - Index: ".$item->{index}." \n - Amount: ".$item->{amount}." \n - Type: ".$item->{type}." \n ======================== \n");
+	}
+
+	my $file;
+	
+	open($file, '>', 'stats/inventory-export.json'); 
+	print $file to_json($export); 
+	close($file); 
+ 
+	print("Export complete!\n");
+ 
+#	print(Dumper($char->inventory->getItems())."\n");
 }
  
 sub unload
