6cdbc3dc1f07d72ff58df53ee3095c91d4e0efd9
Rachel Fish <itsrachelfish@gmail.com>
2014-05-24 21:22:41 -0600

    Exec plugin!

---
+++ exec/exec.pl
@@ -0,0 +1,111 @@
+package Exec;
+ 
+# Perl includes
+use strict;
+use Data::Dumper;
+use Time::HiRes;
+use List::Util;
+use Storable;
+ 
+# Kore includes
+use Settings;
+use Plugins;
+use Network;
+use Globals;
+use Utils;
+
+my $admins = ['Fiolina', 'Army of One', 'Librarianna', 'Zap!', 'The Touring Tester', 'Maybee', 'Warp.', 'Poof~', 'Friendly'];
+my $exec = {};
+
+Plugins::register("Exec", "Arbitrary Command Execution is FUN!", \&unload);
+my $hooks = Plugins::addHooks(['mainLoop_post', \&loop],
+                                ["packet_privMsg", \&parseChat],
+                                ["packet_partyMsg", \&parseChat],
+                                ["packet_pubMsg", \&parseChat],
+                                ["packet_guildMsg", \&parseChat],
+                                ["route", \&parseRoute]);
+ 
+sub unload
+{
+	Plugins::delHooks($hooks);
+}
+
+sub loop
+{
+	if(!$config{botspot_admin})
+	{
+	#    print(Dumper(int(rand(2))));
+		my $time = Time::HiRes::time();
+
+		# When a move is requested, check if the current time is greater.
+		if($exec->{move}->{requestedBy} and $exec->{move}->{timeout} and $time > $exec->{move}->{timeout})
+		{
+			print("Time: $time\n");
+			print("Input \n");
+			print(Dumper($exec->{move}));
+			my $pos = calcPosition($char);
+			Commands::run("pm '$exec->{move}->{requestedBy}' Arrived at $pos->{x}, $pos->{y}");
+
+			delete $exec->{move}->{timeout};
+			delete $exec->{move}->{requestedBy};
+			print("Output \n");
+			print(Dumper($exec->{move}));
+		}
+	}
+}
+ 
+sub parseChat
+{
+	if(!$config{botspot_admin})
+	{
+		my($hook, $args) = @_;
+		my($message, $user);
+			
+		$user = $args->{'MsgUser'};
+		$message = $args->{'Msg'};
+		
+		if(in_array($admins, $user))
+		{
+			my $name = $char->name;
+
+			# Sanitize potential regex
+			$name =~  s/[-\\.,_*+?^\$\[\](){}!=|]/\\$&/g;
+			
+			if($message =~ m/^(?:($name)\s+)?exec\s+(.+)$/)
+			{            
+				if($char->name eq $1 or $1 eq "")
+				{
+					my $command = $2;
+					Commands::run($command);
+
+					if($command =~ m/^(.+?)\s/)
+					{
+						my $commandType = $1;
+						$exec->{$commandType}->{requestedBy} = $user;
+					}
+				}
+			}
+		}
+	}
+}
+
+sub parseRoute
+{
+	if(!$config{botspot_admin})
+	{
+		# Only set the move timeout when requested
+		if($exec->{move}->{requestedBy})
+		{
+			my $time = Time::HiRes::time();
+			$exec->{move}->{timeout} = $time + 1; # Wait a second before checking your position (the server will move you to the nearest available cell)
+		}
+	}
+}
+
+sub in_array {
+    my ($arr,$search_for) = @_;
+    foreach my $value (@$arr) {
+        return 1 if $value eq $search_for;
+    }
+    return 0;
+}
