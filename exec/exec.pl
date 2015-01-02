ac8a411510fb957d37a9d5f76238d37216b234c0
Rachel Fish <itsrachelfish@gmail.com>
2015-01-01 22:55:37 -0700

    Tabs -> Spaces

--- exec/exec.pl
+++ exec/exec.pl
@@ -40,79 +40,78 @@ sub check_admins
  
 sub unload
 {
-	Plugins::delHooks($hooks);
+    Plugins::delHooks($hooks);
 }
 
 sub loop
 {
-	if(!$config{botspot_admin})
-	{
-	#    print(Dumper(int(rand(2))));
-		my $time = Time::HiRes::time();
+    if(!$config{botspot_admin})
+    {
+        my $time = Time::HiRes::time();
 
-		# When a move is requested, check if the current time is greater.
-		if($exec->{move}->{requestedBy} and $exec->{move}->{timeout} and $time > $exec->{move}->{timeout})
-		{
-			print("Time: $time\n");
-			print("Input \n");
-			print(Dumper($exec->{move}));
-			my $pos = calcPosition($char);
-			Commands::run("pm '$exec->{move}->{requestedBy}' Arrived at $pos->{x}, $pos->{y}");
+        # When a move is requested, check if the current time is greater.
+        if($exec->{move}->{requestedBy} and $exec->{move}->{timeout} and $time > $exec->{move}->{timeout})
+        {
+            print("Time: $time\n");
+            print("Input \n");
+            print(Dumper($exec->{move}));
+            my $pos = calcPosition($char);
+            Commands::run("pm '$exec->{move}->{requestedBy}' Arrived at $pos->{x}, $pos->{y}");
 
-			delete $exec->{move}->{timeout};
-			delete $exec->{move}->{requestedBy};
-			print("Output \n");
-			print(Dumper($exec->{move}));
-		}
-	}
+            delete $exec->{move}->{timeout};
+            delete $exec->{move}->{requestedBy};
+            print("Output \n");
+            print(Dumper($exec->{move}));
+        }
+    }
 }
  
 sub parseChat
 {
-	if(!$config{botspot_admin})
-	{
-		my($hook, $args) = @_;
-		my($message, $user);
-			
-		$user = $args->{'MsgUser'};
-		$message = $args->{'Msg'};
+    if(!$config{botspot_admin})
+    {
+        my($hook, $args) = @_;
+        my($message, $user);
+            
+        $user = $args->{'MsgUser'};
+        $message = $args->{'Msg'};
         
-		if(in_array($admins, $user))
-		{
-			my $name = $char->name;
+        if(in_array($admins, $user))
+        {
+            my $name = $char->name;
 
-			# Sanitize potential regex
-			$name =~  s/[-\\.,_*+?^\$\[\](){}!=|]/\\$&/g;
-			
-			if($message =~ m/^(?:($name)\s+)?exec\s+(.+)$/)
-			{            
-				if($char->name eq $1 or $1 eq "")
-				{
-					my $command = $2;
-					Commands::run($command);
+            # Sanitize potential regex
+            $name =~  s/[-\\.,_*+?^\$\[\](){}!=|]/\\$&/g;
+            
+            if($message =~ m/^(?:($name)\s+)?exec\s+(.+)$/)
+            {            
+                if($char->name eq $1 or $1 eq "")
+                {
+                    my $command = $2;
+                    Commands::run($command);
 
-					if($command =~ m/^(.+?)\s/)
-					{
-						my $commandType = $1;
-						$exec->{$commandType}->{requestedBy} = $user;
-					}
-				}
-			}
-		}
-	}
+                    if($command =~ m/^(.+?)\s/)
+                    {
+                        my $commandType = $1;
+                        $exec->{$commandType}->{requestedBy} = $user;
+                    }
+                }
+            }
+        }
+    }
 }
 
 sub parseRoute
 {
-	if(!$config{botspot_admin})
-	{
-		# Only set the move timeout when requested
-		if($exec->{move}->{requestedBy})
-		{
-			my $time = Time::HiRes::time();
-			$exec->{move}->{timeout} = $time + 1; # Wait a second before checking your position (the server will move you to the nearest available cell)
-		}
-	}
+    if(!$config{botspot_admin})
+    {
+        # Only set the move timeout when requested
+        if($exec->{move}->{requestedBy})
+        {
+            my $time = Time::HiRes::time();
+            $exec->{move}->{timeout} = $time + 1; # Wait a second before checking your position (the server will move you to the nearest available cell)
+        }
+    }
 }
 
 sub in_array
